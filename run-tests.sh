cb237634ddec95b5be0b0ac063e16f1a6efb7fb0
Kevin Wang <wy721@qq.com>
2025-07-29 04:28:54 +0000

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -6,16 +6,15 @@ set -e
 pushd sdk/simulator
 ./dstack-simulator &
 SIMULATOR_PID=$!
+trap "kill $SIMULATOR_PID 2>/dev/null || true" EXIT
 echo "Simulator process (PID: $SIMULATOR_PID) started."
 popd
 
 export DSTACK_SIMULATOR_ENDPOINT=$(realpath sdk/simulator/dstack.sock)
+export TAPPD_SIMULATOR_ENDPOINT=$(realpath sdk/simulator/tappd.sock)
 
 echo "DSTACK_SIMULATOR_ENDPOINT: $DSTACK_SIMULATOR_ENDPOINT"
+echo "TAPPD_SIMULATOR_ENDPOINT: $TAPPD_SIMULATOR_ENDPOINT"
 
 # Run the tests
 cargo test
-
-# Kill the simulator after tests finish
-kill $SIMULATOR_PID
-echo "Simulator process (PID: $SIMULATOR_PID) terminated."
